7e60dccf1f2b4716bc6cbfdca070a839bcb627ac
Hermes Netto <hebn.94@gmail.com>
2020-01-09 09:30:55 -0300

    GIT: Add the build folder on track

---
+++ build/main.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var apollo_server_express_1 = require("apollo-server-express");
+var typeDefs_1 = require("./typeDefs");
+var resolvers_1 = require("./resolvers");
+require('dotenv').config();
+var apolloServer = new apollo_server_express_1.ApolloServer({ typeDefs: typeDefs_1.typeDefs, resolvers: resolvers_1.resolvers });
+var app = express_1.default();
+apolloServer.applyMiddleware({ app: app });
+app.listen({ port: process.env.PORT || 4000 }, function () {
+    return console.log("\uD83D\uDE80 GraphQL Server ready at http://localhost:4000" + apolloServer.graphqlPath);
+});
---
+++ build/resolvers/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.resolvers = {
+    Query: {
+        hello: function () { return 'Hello world!'; }
+    }
+};
